33b7582c4456950f8985b79e7f244cdbbc3fcac6
Victoria <victoria.xiao.19@ucl.ac.uk>
2023-02-15 12:00:59 +0000

    only be able to ice on cake semi working. ISSUE: if pen leaves cake after starting to draw, it will continue to draw

--- Assets/Scripts/IcingBrush.cs
+++ Assets/Scripts/IcingBrush.cs
@@ -12,7 +12,9 @@ public class IcingBrush : MonoBehaviour, IGraspable, IUseable
     private Transform nib;
     private Material drawingMaterial;
     private GameObject currentDrawing;
+    private GameObject nib_obj;
     private bool owner;
+    private bool isTouchingCake = false;
 
     public void Grasp(Hand controller)
     {
@@ -39,16 +41,19 @@ public class IcingBrush : MonoBehaviour, IGraspable, IUseable
     private void BeginDrawing() 
     {
         currentDrawing = new GameObject("Drawing");
-        var trail = currentDrawing.AddComponent<TrailRenderer>();
-        trail.time = Mathf.Infinity;
-        trail.material = drawingMaterial;
-        trail.startWidth = .05f;
-        trail.endWidth = .05f;
-        trail.minVertexDistance = .02f;
+        if (isTouchingCake)
+        {
+            var trail = currentDrawing.AddComponent<TrailRenderer>();
+            trail.time = Mathf.Infinity;
+            trail.material = drawingMaterial;
+            trail.startWidth = .05f;
+            trail.endWidth = .05f;
+            trail.minVertexDistance = .02f;
 
-        currentDrawing.transform.parent = nib.transform;
-        currentDrawing.transform.localPosition = Vector3.zero;
-        currentDrawing.transform.localRotation = Quaternion.identity;
+            currentDrawing.transform.parent = nib.transform;
+            currentDrawing.transform.localPosition = Vector3.zero;
+            currentDrawing.transform.localRotation = Quaternion.identity;
+        }
     } 
     
     private void EndDrawing()
@@ -88,16 +93,16 @@ public class IcingBrush : MonoBehaviour, IGraspable, IUseable
             EndDrawing();
         }
     }
-
     void Start()
     {
         nib = transform.Find("PipingTip/Nib");
+        nib_obj = GameObject.Find("Nib");
         context = NetworkScene.Register(this);
-        var shader = Shader.Find("Particles/Standard Unlit");
+        var shader = Shader.Find("Particles/Standard Surface");
         drawingMaterial = new Material(shader);
+        drawingMaterial.SetColor("_Color", Color.red); // sets colour, TODO: add to menu
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         if (owner)
@@ -113,6 +118,8 @@ public class IcingBrush : MonoBehaviour, IGraspable, IUseable
             transform.position = grasped.transform.position;
             transform.rotation = grasped.transform.rotation;
         }
+
+        isTouchingCake = nib_obj.GetComponent<NibOnCake>().isTouching;
     }
 
 }
