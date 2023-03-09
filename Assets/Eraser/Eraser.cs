dd514fd3bc8461c2650e41bc323d7c27d4bcd64d
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-03-09 16:00:40 +0000

    eraser prefab

---
+++ Assets/Eraser/Eraser.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ubiq.XR;
+using Ubiq.Messaging;
+
+public class Eraser : MonoBehaviour, IGraspable
+{   
+    Hand grasped;
+    NetworkContext context;
+    Vector3 lastPosition;
+    
+    private bool owner; 
+    private Rigidbody rigidBody;
+
+    public void Grasp(Hand controller)
+    {
+        owner = true;
+        grasped = controller;
+    }
+
+    public void Release(Hand controller)
+    {
+        owner = false;
+        grasped = null;
+    }
+
+    private void Awake()
+    {
+        owner = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        context = NetworkScene.Register(this);
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+
+    private struct Message
+    {
+        public Vector3 position;
+    }
+
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        // Parse the message
+        var msg = message.FromJson<Message>();
+      
+        // Use the message to update the Component
+        transform.position = msg.position;
+
+        // Make sure the logic in Update doesn't trigger as a result of this message
+        lastPosition = transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Destroy(collision.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (owner)
+        {
+            if(lastPosition != transform.position)
+            {
+                lastPosition = transform.position;
+                 context.SendJson(new Message()
+                {
+                    position = transform.localPosition,
+                });
+
+            }
+        }
+
+    }
+}
