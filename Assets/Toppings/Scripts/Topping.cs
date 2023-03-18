7818c9738fa5e7cd9ebd3992d17fa2822e5a7563
Victoria <victoria.xiao.19@ucl.ac.uk>
2023-03-18 01:30:44 +0000

    use oncollisionenter instead of ontriggerenter for toppings (makes it land nicer on the cake)

--- Assets/Toppings/Scripts/Topping.cs
+++ Assets/Toppings/Scripts/Topping.cs
@@ -20,13 +20,13 @@ public class Topping : MonoBehaviour
 
 
     // Detects collision with floor and cake and acts accordingly
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Cake")
+        if (other.collider.tag == "Cake")
         {
             rigidBody.isKinematic = true; // remove physics of topping
         }
-        if (other.gameObject.tag == "Floor")
+        if (other.collider.tag == "Floor")
         {
             Destroy(gameObject, 5); // destroy gameobject after 5s of touching floor
         }
