cfee742956c41e77d20c38715106a4cee3943b39
ShaheerA2311 <ShaheerA2311@users.noreply.github.com>
2023-03-19 21:34:24 +0000

    networking but cs1022 eof error idek help

--- Assets/Sprinkles/SprinklesTool.cs
+++ Assets/Sprinkles/SprinklesTool.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Ubiq.Messaging;
+using Ubiq.Spawning;
 using Ubiq.XR;
+using UnityEngine;
 
 public class SprinklesTool : MonoBehaviour, IGraspable, IUseable
 {
@@ -10,13 +12,13 @@ public class SprinklesTool : MonoBehaviour, IGraspable, IUseable
     private Vector3 velocity;
     public GameObject sprinklePrefab;
 
+    private bool isPlacing = false;
+
     private objectPoolManager ObjectPoolManager;
 
     void Start()
     {
-        
-        //ObjectPoolManager = GameObject.FindObjectOfType<objectPoolManager>();
-
+        context = NetworkScene.Register(this);
     }
 
     public void Grasp(Hand controller)
@@ -28,19 +30,32 @@ public class SprinklesTool : MonoBehaviour, IGraspable, IUseable
     {
         hand = null;
     }
-
+    
     public void UnUse(Hand controller)
     {
 
     }
-
+    
     public void Use(Hand controller)
     {
         GameObject spawnedSprinkle = Instantiate(sprinklePrefab, transform.position, transform.rotation);
-
         //GameObject spawnedSprinkle = ObjectPoolManager.GetObjectFromPool(sprinklePrefab, transform.position, transform.rotation);
-
         spawnedSprinkle.GetComponent<SprinkleController>().setVelocity(velocity * 2); // add networking to spawn as well on other clients
+        private bool isPlacing = true;
+    }
+    
+    public struct Message
+        /* message contains
+            position of topping
+            rotation of topping
+            what topping it is (name)
+            whether user used tool (placing)
+        */
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public string name;
+        public bool placing;
     }
 
     void Update()
@@ -51,5 +66,36 @@ public class SprinklesTool : MonoBehaviour, IGraspable, IUseable
             velocity = ((transform.position - previous)) / Time.deltaTime;
             previous = transform.position;
         }
+            if (owner) // send message to other players updating them of topping tool's behaviour
+            {
+                context.SendJson(new Message()
+                {
+                    //position = transform.position,
+                    //rotation = transform.rotation,
+                    //name = transform.name,
+                    placing = isPlacing
+                });
+            }
+
     }
+    
 }
+
+    //Can you take a look at this part pls? >> giving error when removing '}' in line 98
+    /*
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<Message>();
+
+        if (msg.name == transform.name)
+        {
+            if (msg.placing)
+            {
+                Use();
+            }
+            //transform.position = msg.position;
+            //transform.rotation = msg.rotation;
+        }
+    }
+    */
+
